10ac4c3472e96eda0db3df87e2f0fee7eb1ab19b
Md. Tofayel Islam <tofayelislam19986@gmail.com>
2024-07-17 01:07:41 +0600

    --create slice

---
+++ src/app/store.js
@@ -0,0 +1,9 @@
+
+import { configureStore } from "@reduxjs/toolkit";
+import videosReducer from "../features/videos/videosSlice";
+
+export const store = configureStore({
+    reducer: {
+        videos: videosReducer,
+    },
+});
